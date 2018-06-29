f776f35c2cea4ee26992ceb157dc5569cd49e35f
Adam Young <ayoung@redhat.com>
2018-06-29 19:00:39 -0400

    Add Endpoint table

---
+++ posse/alembic/versions/fa1ee62535b8_create_endpoint_table.py
@@ -0,0 +1,27 @@
+"""create endpoint table
+
+Revision ID: fa1ee62535b8
+Revises: d749a28cdae2
+Create Date: 2018-06-29 18:58:49.931290
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa1ee62535b8'
+down_revision = 'd749a28cdae2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    endpoint  = op.create_table(
+        'endpoint', 
+        sa.Column('id', sa.String(length=64), primary_key=True),
+        sa.Column('service_id', sa.String(length=64))
+
+
+def downgrade():
+    pass
