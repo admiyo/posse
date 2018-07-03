bd108716e5fb95bb894f4a43b58ea824462fc096
Adam Young <ayoung@redhat.com>
2018-07-02 23:14:19 -0400

    Upload works for JSON and YAML

--- posse/app.py
+++ posse/app.py
@@ -1,3 +1,10 @@
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+    
+
 from flask import Flask
 from flask import render_template
 from flaskext.mysql import MySQL
@@ -9,17 +16,18 @@ from flask_restplus import Api
 from flask_restplus import Resource
 
 
+
 app = Flask(__name__)
 
 
 if True:
-    mysql = MySQL(cursorclass=DictCursor)
+    dbconn = MySQL(cursorclass=DictCursor)
     # MySQL configurations
     app.config['MYSQL_DATABASE_USER'] = 'posse'
     app.config['MYSQL_DATABASE_PASSWORD'] = 'posse'
     app.config['MYSQL_DATABASE_DB'] = 'posse'
     app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
-    mysql.init_app(app)
+    dbconn.init_app(app)
 else:
     pass
 
@@ -34,22 +42,65 @@ def showSignUp():
 
 @api.route("/services")
 @api.doc()
-class Services(Resource):
+class Service(Resource):
     def get(self):
-        conn = mysql.connect()
+        conn = dbconn.connect()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM service")
         data = cursor.fetchall() 
         if not data:
             data = dict()
         return data;
+
+
+@api.route("/service/<service_id>/policy")
+@api.doc()
+class ServicePolicy(Resource):
+    def get(self, service_id):
+        conn = dbconn.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM policy_line where service_id = %s" % service_id)
+        data = cursor.fetchall() 
+        if not data:
+            data = dict()
+        return data;
     
+    def put(self, service_id):
+        conn = dbconn.connect()
+        cursor = conn.cursor()
+        if request.content_type == 'text/x-yaml':
+            policies = yaml.load(request.data ,Loader=Loader)
+        else:
+            policies = request.json
+
+        default = ''
+        for k, v in policies.items():
+
+            if k == 'default':
+                default = v
+            if v == '':
+                check_string = "rule:default"
+            else:
+                check_string = v
+
+            t = {'service_id': service_id,
+                 'name':k,
+                 'check_string':check_string }
+            result = cursor.execute(" insert into policy_line  values ('%(service_id)s', '%(name)s', 'description text', 'path text', '%(check_string)s', 'methods text', 'scopes text')" % t)
+        try:
+            conn.commit()
+            return self.get('service_id'), 201
+        except Exception as e:
+            print("Problem inserting into db: " + str(e))
+            return "", 500
+
+
 
 @api.route("/endpoints")
 @api.doc()
 class Endpoints(Resource):
     def get(self):
-        conn = mysql.connect()
+        conn = dbconn.connect()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM endpoint")
         data = cursor.fetchall() 
@@ -58,11 +109,11 @@ class Endpoints(Resource):
         return data;
 
 
-@api.route("/policy_lines")
+@api.route("/policy")
 @api.doc()
 class PolicyLines(Resource):
     def get(self):
-        conn = mysql.connect()
+        conn = dbconn.connect()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM policy_line")
         data = cursor.fetchall()
@@ -75,7 +126,7 @@ class PolicyLines(Resource):
 @api.doc()
 class PolicyLine(Resource):
     def get(self):
-        conn = mysql.connect()
+        conn = dbconn.connect()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM policy_line")
         data = cursor.fetchone()
@@ -85,7 +136,7 @@ class PolicyLine(Resource):
 
     
     def put(self, id, policy):
-        conn = mysql.connect()
+        conn = dbconn.connect()
         cursor = conn.cursor()
         cursor.execute(" insert into policy_line  values ('service_id', 'name', 'description text', 'path text', 'check_string text', 'methods text', 'scopes text'    )")
         return create_object(), 201
